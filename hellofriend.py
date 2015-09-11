ee71345ada2916a365fa5c2e5e2ebce5d1da978e
grantmorhun <grantmorhun@hanovernorwichschools.org>
2015-09-11 14:23:02 -0400

    Updated from Brython Server: 9/11/2015 2:23:01 PM

--- hellofriend.py
+++ hellofriend.py
@@ -20,6 +20,6 @@ on the name and age that the user enters.
 user= input("Tell me your name!")
 print("Hi {0}!".format(user))
 user=input("how old are you {0}?".format(user))
-int=(format("num"))
-int=(24-{1})
-print("Well {0}, python is {2} years older than you!")
+nums="24-{1}"
+int(nums) 
+print("Well {0}, python is 'nums' years older than you!")
