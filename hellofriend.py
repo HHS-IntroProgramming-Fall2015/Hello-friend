e8e6feb6f02cb8a18dffbf5a94d7b3d5bb105a6f
averywallis <averybwallis@gmail.com>
2016-01-22 14:31:06 -0500

    Update hellofriend.py

--- hellofriend.py
+++ hellofriend.py
@@ -1,19 +1,27 @@
 """
+
 hellofriend.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Avery Wallis
+Credit: None
 
-Assignment:
-
-Write and submit an interactive Python program that asks for the user's name and age, 
-then prints how much older Python is than the user (based on a simple comparison of 
-birth year). Python's first public release occurred in 1991. Something like this:
-
-Please tell me your name: Guido
-Please tell me your age: 16
-Hello, Guido. Python is 8 years older than you are!
-
-Note that the text: "Guido" and "16" are entered by the user running the program. 
-The final line ("Hello...") is generated dynamically when you run the program, based 
-on the name and age that the user enters.
 """
+
+a = input('What is your name?')
+b = input('How old are you?')
+b = float(b)
+d = (25-b)
+e = "Hello, "
+f = ". I am "
+g = " years older than you!"
+h = " years younger than you!"
+i = "as old as you are!"
+
+if d > 0:
+    print(e + a + f + str(d) + g)
+    
+if d < 0:
+    d = abs(d)
+    print(e + a + f + str(d) + h)
+    
+if d == 0:
+    print(e + a + f + i)
