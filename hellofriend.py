13058e8f8ce95f7bdeb0b2d1b2636410dc261b9d
averywallis <averybwallis@gmail.com>
2016-01-22 14:50:25 -0500

    Updated from Brython Server: 1/22/2016 2:50:25 PM

--- hellofriend.py
+++ hellofriend.py
@@ -6,14 +6,14 @@ Credit: None
 
 """
 
-a = input('Please tell me your name:')
-b = input('Please tell me your age:')
+a = input('Please tell me your name: ')
+b = input('Please tell me your age: ')
 b = int(b)
 d = (25-b)
 e = "Hello, "
-f = ". I am "
-g = " years older than you!"
-h = " years younger than you!"
+f = ". Python is "
+g = " years older than you are!"
+h = " years younger than you are!"
 i = "as old as you are!"
 
 if d > 0:
