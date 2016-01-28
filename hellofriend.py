46ecd1a9778e1ec21f3d9d61ffdee7cf8bbbf76c
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-01-28 14:34:33 -0500

    Updated from Brython Server: 1/28/2016 2:34:33 PM

--- hellofriend.py
+++ hellofriend.py
@@ -21,6 +21,6 @@ x = str(input("Please tell me your name: "))
 y = int(input("Please tell me your age: "))
 age=25
 agediff=age-y
+print(agediff)
 
-
-print("Hello, "+x+". Python is "+agediff+" than you are!")
+print("Hello, "+x+". Python is "+agediff+" years older than you are!")
