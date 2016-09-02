370fcf128156125141667614c08acc77a158cd40
DRBarnum <douglasbarnum@hanovernorwichschools.org>
2016-09-02 13:58:14 -0400

    Updated from Brython Server: 9/2/2016 1:58:14 PM

--- hellofriend.py
+++ hellofriend.py
@@ -1,7 +1,7 @@
 """
 hellofriend.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Robbie
+Credit: Introuction to Github with Runpython and Introduction to Runpython
 
 Assignment:
 
