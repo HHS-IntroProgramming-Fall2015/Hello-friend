3ff7b557f4cdeac20df53d290bf18fba0cd8fbec
Aqkotz <andy.lyme@mac.com>
2016-09-07 13:11:03 -0400

    Updated from Brython Server: September 7, 2016 1:11:02 PM EDT

--- hellofriend.py
+++ hellofriend.py
@@ -20,7 +20,7 @@ on the name and age that the user enters.
 name=input("Please tell me your name: ")
 age=input("Please tell me your age: ")
 agenum=int(age)
-YOB=2016-agenum
-agediff=YOB-1992
+YOB=2015-agenum
+agediff=YOB-1991
 agediffstr=str(agediff)
 print ("Hello, " + name + ". Python is " + agediffstr + " years older than you are!")
