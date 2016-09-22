364ff46955350ade60a2ca7d9f8b3b1f9a4bae03
xNimbleNavigatorx <alexandrehankel@hanovernorwichschools.org>
2016-09-22 00:19:51 -0400

    Updated from Brython Server: 9/22/2016 12:19:47 AM

--- hellofriend.py
+++ hellofriend.py
@@ -1,19 +1,23 @@
 """
 hellofriend.py
-Author: <your name here>
-Credit: <list sources used, if any>
-
+Author: <xNimbleNavigatorx>
+Credit: <not kezar, github tutorial>
 Assignment:
-
 Write and submit an interactive Python program that asks for the user's name and age, 
 then prints how much older Python is than the user (based on a simple comparison of 
 birth year). Python's first public release occurred in 1991. Something like this:
-
 Please tell me your name: Guido
 Please tell me your age: 16
 Hello, Guido. Python is 8 years older than you are!
-
 Note that the text: "Guido" and "16" are entered by the user running the program. 
 The final line ("Hello...") is generated dynamically when you run the program, based 
 on the name and age that the user enters.
 """
+
+name = input("Please tell me your name: ")
+age = input("please tell me your age: ")
+
+s2 = "Hello, " + name + ". Python is {0} years older than you are!"
+
+print(s2.format(25-int(age)))
+
