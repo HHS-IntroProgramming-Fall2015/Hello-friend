b5d6c9cff727fcb51797ff39eb951d443e89c5a4
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-01-27 14:38:59 -0500

    Updated from Brython Server: 1/27/2017 2:38:58 PM

--- hellofriend.py
+++ hellofriend.py
@@ -8,4 +8,4 @@ Credit: worked with Brendan
 name=input("Please tell me your name:")
 age=input("Please tell me your age:")
 s= ("Hello, {0}. Python is {1} years older than you are!")
-print(s.format(name,(25-(int((age))))))
+print(s.format(name,(26-(int((age))))))
