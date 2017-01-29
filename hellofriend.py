c7a405f96b5cfb5a044f642a8dabd8dae8d3c27f
willl4 <williamlaycock@hanovernorwichschools.org>
2017-01-29 00:08:10 -0500

    Updated from Brython Server: January 29, 2017 12:08:12 AM EST

--- hellofriend.py
+++ hellofriend.py
@@ -17,8 +17,8 @@ Note that the text: "Guido" and "16" are entered by the user running the program
 The final line ("Hello...") is generated dynamically when you run the program, based 
 on the name and age that the user enters.
 """
-name = input("please tell me your name: ")
-age = input("please tell me your age: ")
+name = input("Please tell me your name: ")
+age = input("Please tell me your age: ")
 s1 = name
 s2 = age
 s3 = "Hello, {0}. Python is {1} years older than you are!"
