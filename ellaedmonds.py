247baf9dcd9e00a9d1eb456f4aaebb3f2cb905d6
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-09-02 16:26:48 -0400

    Update and rename hellofriend.py to ellaedmonds.py

--- ellaedmonds.py
+++ ellaedmonds.py
@@ -17,3 +17,13 @@ Note that the text: "Guido" and "16" are entered by the user running the program
 The final line ("Hello...") is generated dynamically when you run the program, based 
 on the name and age that the user enters.
 """
+name = input('Please tell me your name: ',)
+a = float(input('Please tell me your age: ',))
+b = 2018-a
+c = b-1991
+if c > 0:
+    print('Hello, ',name,'. Python is ', c,' years older than you are!')
+if c < 0:
+    print('Hello, ',name,'. Python is ', -c,' years younger than you are!')
+elif c == 0:
+    print('Hello, ',name,'. Python is the same age as you!')
