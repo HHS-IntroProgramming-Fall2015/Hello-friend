ed26460300c9efceeb04e961180242b2541e934b
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-09-04 12:35:26 -0400

    Updated from Brython Server: 9/4/2018 12:35:34 PM

--- hellofriend.py
+++ hellofriend.py
@@ -19,4 +19,4 @@ on the name and age that the user enters.
 """
 name = input("Please tell me your name: ")
 s2 = input("Please tell me your age: ")
-print ("Hello, "+ name +". Python is 27 - "+ s2 +" years older than you are!")
+print ("Hello, "+ name +". Python is 27 - "+ int("s2") +" years older than you are!")
