fdececaec52ad0eb19a3f1d046e7b7f84b6a7c84
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-09-04 11:37:51 -0400

    Updated from Brython Server: 9/4/2018 11:37:51 AM

--- joharihellofriend.py
+++ joharihellofriend.py
@@ -1,6 +1,6 @@
 a = input("Please tell me your name: ") 
 b = input("Please tell me your age: ") 
 int(b)<= 27 
-print("Hello," + a) 
+print("Hello," + a + ".") 
 s = "Python is {0} years older than you!" 
 print(s.format(27-int(b)))
