b15ac63ef8d174f95636394688854a936c9782c5
quonchuck <42871969+quonchuck@users.noreply.github.com>
2018-09-04 12:04:12 -0400

    Updated from Brython Server: 9/4/2018 12:04:12 PM

--- hellofriend.py
+++ hellofriend.py
@@ -1,9 +1,8 @@
 """
 hellofriend.py
-Author: <your name here>
-Credit: <list sources used, if any>
-
-Assignment:
+Author: Quincy McBride
+Credit: Myself
+Assignment: Hello Friend
 
 Write and submit an interactive Python program that asks for the user's name and age, 
 then prints how much older Python is than the user (based on a simple comparison of 
@@ -17,3 +16,9 @@ Note that the text: "Guido" and "16" are entered by the user running the program
 The final line ("Hello...") is generated dynamically when you run the program, based 
 on the name and age that the user enters.
 """
+
+name=input("Please tell me your name: " )
+age=input("Please tell me your age: ")
+a=2018-age
+b= a-1991
+print("Hello, " +name+ ". Python is " +str(b)+  "years older than you are!")
