ecd61f2e555e4bfcdb0c6ca578dd1cda993d4af2
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-09-04 12:47:00 -0400

    Updated from Brython Server: 9/4/2018 12:47:00 PM

--- hellofriend.py
+++ hellofriend.py
@@ -1,3 +1,22 @@
+"""
+hellofriend.py
+Author: Eric Goodney
+Credit: Peers
+
+Assignment:
+
+Write and submit an interactive Python program that asks for the user's name and age, 
+then prints how much older Python is than the user (based on a simple comparison of 
+birth year). Python's first public release occurred in 1991. Something like this:
+
+Please tell me your name: Guido
+Please tell me your age: 16
+Hello, Guido. Python is 8 years older than you are!
+
+Note that the text: "Guido" and "16" are entered by the user running the program. 
+The final line ("Hello...") is generated dynamically when you run the program, based 
+on the name and age that the user enters.
+"""
 name= str(input("Please tell me your name: "))
 age= int(input("Please tell me your age: "))
 a=age
@@ -15,22 +34,5 @@ print("Hello, " + name + ". Python is " + c + " years older than you are!")
 
 
 
-"""
-hellofriend.py
-Author: <your name here>
-Credit: <list sources used, if any>
 
-Assignment:
 
-Write and submit an interactive Python program that asks for the user's name and age, 
-then prints how much older Python is than the user (based on a simple comparison of 
-birth year). Python's first public release occurred in 1991. Something like this:
-
-Please tell me your name: Guido
-Please tell me your age: 16
-Hello, Guido. Python is 8 years older than you are!
-
-Note that the text: "Guido" and "16" are entered by the user running the program. 
-The final line ("Hello...") is generated dynamically when you run the program, based 
-on the name and age that the user enters.
-"""
