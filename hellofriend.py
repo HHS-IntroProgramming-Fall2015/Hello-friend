2ea8752e62447d59a7e7bd17f15e540649500e3e
slushey <42842766+slushey@users.noreply.github.com>
2018-09-04 12:11:14 -0400

    Updated from Brython Server: 9/4/2018 12:11:13 PM

--- hellofriend.py
+++ hellofriend.py
@@ -20,7 +20,7 @@ on the name and age that the user enters.
 import datetime
 year = datetime.datetime.now().year
 name = input('Please tell me your name: ')
-age = float(input('Please tell me your age: '))
+age = int(input('Please tell me your age: '))
 c = year-age
 d = c-1991
 print('Hello, '  +str(name) +str('. Python is ') +str(d) +str(' years older than you are!'))
