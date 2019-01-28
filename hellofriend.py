52d15bec46d1c4070304481e76d13cb8e03ba5f9
CuPyrtch <46973325+CuPyrtch@users.noreply.github.com>
2019-01-28 14:16:14 -0500

    Updated from Brython Server: 1/28/2019 2:16:13 PM

--- hellofriend.py
+++ hellofriend.py
@@ -1,9 +1,13 @@
 """
 hellofriend.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Curtis Pych
+Credit: https://www.codesdope.com/python-readreadread/
+https://snakify.org/en/lessons/print_input_numbers/
+https://www.techwalla.com/articles/how-to-convert-int-to-string-in-python
+https://linuxconfig.org/working-with-number-variables-in-python
+https://en.wikibooks.org/wiki/Python_Programming/Basic_Math, 
 
-Assignment:
+Assignment: Hello Friend
 
 Write and submit an interactive Python program that asks for the user's name and age, 
 then prints how much older Python is than the user (based on a simple comparison of 
@@ -17,3 +21,9 @@ Note that the text: "Guido" and "16" are entered by the user running the program
 The final line ("Hello...") is generated dynamically when you run the program, based 
 on the name and age that the user enters.
 """
+
+name = input("Please tell me your name: ")
+b = input("Please tell me your age: ")
+c = int(b)
+s = 28-c
+print("Hello " + name + ", Python is " + str(s) + " Years older than you!")
