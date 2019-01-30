20fc24d18078e08c6fe0b547493df0234685717e
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-01-30 14:02:06 -0500

    Update hellofriend.py

--- hellofriend.py
+++ hellofriend.py
@@ -1,7 +1,7 @@
 """
 hellofriend.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Esther Hacker
+Credit: N/A
 
 Assignment:
 
@@ -17,3 +17,9 @@ Note that the text: "Guido" and "16" are entered by the user running the program
 The final line ("Hello...") is generated dynamically when you run the program, based 
 on the name and age that the user enters.
 """
+name = input("Please tell me your name: ")
+age = input("Please tell me your age: ")
+
+years_older = 28 - int(age)
+
+print("Hello, " + name + ". Python is " + str(years_older) + " years older than you are!")
